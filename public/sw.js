2adcea3a4b8d97b05024b4f0fcc21228f85701f7
Dorothea714 <gonghuimin714@outlook.com>
2026-06-29 00:04:13 +0800

    添加 PWA 支持，可安装为桌面 App

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+// 哲学感悟分析器 Service Worker
+const CACHE_NAME = 'philosophy-reflect-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
